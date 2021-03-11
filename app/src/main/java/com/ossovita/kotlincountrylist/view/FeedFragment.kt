eb6837b0bd84fcdca1906dea827ab5a19d4be67d
sancaryum <yasineryigit737@gmail.com>
2021-03-12 02:48:29 +0300

    Before Country View Model

--- app/src/main/java/com/ossovita/kotlincountrylist/view/FeedFragment.kt
+++ app/src/main/java/com/ossovita/kotlincountrylist/view/FeedFragment.kt
@@ -5,10 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ossovita.kotlincountrylist.R
+import com.ossovita.kotlincountrylist.adapter.CountryAdapter
+import com.ossovita.kotlincountrylist.model.Country
+import com.ossovita.kotlincountrylist.viewmodel.FeedViewModel
+import kotlinx.android.synthetic.main.fragment_feed.*
 
 
 class FeedFragment : Fragment() {
+    //private final FeedViewModel viewModel; aşağıdaki bu demektir
+    private lateinit var viewModel : FeedViewModel
+    private val countryAdapter = CountryAdapter(arrayListOf())
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,12 +37,56 @@ class FeedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(FeedViewModel::class.java)
+        viewModel.refreshData()
+
+        countryList.layoutManager = LinearLayoutManager(context)
+        countryList.adapter = countryAdapter
+
+
         /*
         fragment_button.setOnClickListener {
             val action = FeedFragmentDirections.actionFeedFragmentToCountryFragment();
             Navigation.findNavController(view).navigate(action);
 
         }*/
+
+        observeLiveData()
+    }
+
+    //veri çekmek için bir metod
+    fun observeLiveData(){
+        //viewModel'dan gelen countries listesini dinliyoruz
+        viewModel.countries.observe(viewLifecycleOwner, Observer {countries->
+            //countries boş değilse visible yap
+            countries?.let {
+                countryList.visibility=View.VISIBLE
+                countryAdapter.updateCountryList(countries)
+            }
+        })
+        //viewModel'den gelen countryError boolean'ını dinliyoruz.
+        //Lifecycleowner parametresi olarak viewlifecycleowner veriyoruz
+        viewModel.countryError.observe(viewLifecycleOwner, Observer {error->
+            error?.let {
+                if(it){//hata varsa hata mesajını göster
+                    countryError.visibility=View.VISIBLE
+                }else{
+                    countryError.visibility=View.GONE
+                }
+            }
+        })
+        //viewModel'dan gelen countryLoading boolean'ını dinliyoruz
+        viewModel.countryLoading.observe(viewLifecycleOwner, Observer {loading->
+            loading?.let {
+                if(it){
+                    countryLoading.visibility=View.VISIBLE
+                    countryList.visibility=View.GONE
+                    countryError.visibility=View.GONE
+                }else{
+                    countryLoading.visibility=View.GONE
+                }
+            }
+        })
     }
 
 
